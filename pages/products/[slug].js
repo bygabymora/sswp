75fbf273317daed81c66193b38f37760fe5974b8
bygabymora <bygabymora@gmail.com>
2023-11-29 13:35:04 -0500

    Fix bug in login functionality

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -20,6 +20,10 @@ export default function ProductScreen(props) {
   const [showPopup, setShowPopup] = useState(false);
   const [isOutOfStock, setIsOutOfStock] = useState(false);
   const [qty, setQty] = useState(1);
+  const [currentImage, setCurrentImage] = useState(product.image);
+  const changeImage = (image) => {
+    setCurrentImage(image);
+  };
 
   if (!product) {
     return (
@@ -69,118 +73,133 @@ export default function ProductScreen(props) {
       <div className="product-grid">
         <div className="product-image">
           <Image
-            src={`${product.image}`}
+            src={currentImage}
             alt={product.reference}
-            width={640}
-            height={640}
+            width={500}
+            height={800}
           />
         </div>
-        <div className="">
-          <ul>
-            <li>
-              <h1 className="text-xl font-bold">{product.name}</h1>
-            </li>
-            <li>
-              <h1 className="text-xl">{product.description}</h1>
-            </li>
-          </ul>
+        <div className="image-thumbnails">
+          {[product.image, product.image2, product.image3].map((img, index) => (
+            <Image
+              key={index}
+              src={img}
+              alt={`Thumbnail ${index}`}
+              onClick={() => changeImage(img)}
+              style={{ width: '100px', height: '100px', cursor: 'pointer' }}
+              width={100}
+              height={100}
+            />
+          ))}
         </div>
-        <div>
-          <div className="card p-5">
-            <div className="mb-2 flex items-center justify-center lg:block">
-              <div className="font-bold mt-4">Cantidad &nbsp;</div>
-              <div className="flex items-center flex-row">
-                <button
-                  className="border px-2 py-1 card"
-                  onClick={() => setQty(Math.max(1, qty - 1))}
-                  disabled={qty <= 1}
-                >
-                  -
-                </button>
-                <span className="px-1 mt-4">{isOutOfStock ? 0 : qty}</span>
-                <button
-                  className="border px-2 py-1 card"
-                  onClick={() => {
-                    if (qty < product.countInStock) {
-                      setQty(qty + 1);
-                    } else {
-                      alert(
-                        `Sorry,  we do not have any additional units of ${product.manufacturer} ${product.slug} at this moment`
-                      );
-                    }
-                  }}
-                  disabled={isOutOfStock}
-                >
-                  +
-                </button>
-              </div>
-            </div>
-            <div className="mb-2 flex justify-between">
-              <div className="font-bold">Precio</div>
-              <div className="text-2xl">
-                ${formatNumberWithDots(product.price)}
-              </div>
-            </div>
-            <div className="mb-2 flex justify-between">
-              <div className="font-bold">Inventario</div>
-              &nbsp;
-              <div className="text-lg ">
-                {isOutOfStock || product.countInStock === 0
-                  ? 'Sin inventario'
-                  : 'En inventario'}
-              </div>
-            </div>
-            <button
-              className="primary-button cart-button"
-              type="button"
-              onClick={addToCartHandler}
-              disabled={product.countInStock === 0 || isOutOfStock}
-            >
-              {isOutOfStock ? 'Fuera de inventario' : 'Añadir al carrito'}
-            </button>
-            {showPopup && (
-              <div className="popup">
-                <div className="popup-content">
-                  <p>item añadido al carrito.</p>
-                  <br />
-                  <div className="flex gap-1 justify-evenly">
-                    <button
-                      className="primary-button w-1/2 text-xs text-left"
-                      onClick={continueShoppingHandler}
-                    >
-                      Continuar comprando
-                    </button>
-                    <button
-                      className=" flex primary-button w-1/2 text-xs text-left items-center"
-                      onClick={goToCartHandler}
-                    >
-                      <p>Ir al carrito</p> &nbsp;
-                      <BsCart2 className="text-2xl" />
-                    </button>
-                  </div>
+        <div className="product-info">
+          <div className="">
+            <ul>
+              <li>
+                <h1 className="text-xl font-bold">{product.name}</h1>
+              </li>
+              <li>
+                <h1 className="text-xl">{product.description}</h1>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <div className="card p-5">
+              <div className="mb-2 flex items-center justify-center lg:block">
+                <div className="font-bold mt-4">Cantidad &nbsp;</div>
+                <div className="flex items-center flex-row">
+                  <button
+                    className="border px-2 py-1 card"
+                    onClick={() => setQty(Math.max(1, qty - 1))}
+                    disabled={qty <= 1}
+                  >
+                    -
+                  </button>
+                  <span className="px-1 mt-4">{isOutOfStock ? 0 : qty}</span>
+                  <button
+                    className="border px-2 py-1 card"
+                    onClick={() => {
+                      if (qty < product.countInStock) {
+                        setQty(qty + 1);
+                      } else {
+                        alert(
+                          `Sorry,  we do not have any additional units of ${product.manufacturer} ${product.slug} at this moment`
+                        );
+                      }
+                    }}
+                    disabled={isOutOfStock}
+                  >
+                    +
+                  </button>
                 </div>
               </div>
-            )}
-            {isOutOfStock && (
-              <form className="text-center mt-3 ">
-                <label className="mt-3 font-bold ">
-                  Únete a nuestra lista de espera
-                </label>
-                <input
-                  type="text"
-                  placeholder="Nombre"
-                  className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                />
-                <input
-                  type="email"
-                  placeholder="Email"
-                  className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                />
-                <button className="primary-button mt-3" type="submit">
-                  Enviar
-                </button>
-              </form>
-            )}
+              <div className="mb-2 flex justify-between">
+                <div className="font-bold">Precio</div>
+                <div className="text-2xl">
+                  ${formatNumberWithDots(product.price)}
+                </div>
+              </div>
+              <div className="mb-2 flex justify-between">
+                <div className="font-bold">Inventario</div>
+                &nbsp;
+                <div className="text-lg ">
+                  {isOutOfStock || product.countInStock === 0
+                    ? 'Sin inventario'
+                    : 'En inventario'}
+                </div>
+              </div>
+              <button
+                className="primary-button cart-button"
+                type="button"
+                onClick={addToCartHandler}
+                disabled={product.countInStock === 0 || isOutOfStock}
+              >
+                {isOutOfStock ? 'Fuera de inventario' : 'Añadir al carrito'}
+              </button>
+              {showPopup && (
+                <div className="popup">
+                  <div className="popup-content">
+                    <p>item añadido al carrito.</p>
+                    <br />
+                    <div className="flex gap-1 justify-evenly">
+                      <button
+                        className="primary-button w-1/2 text-xs text-left"
+                        onClick={continueShoppingHandler}
+                      >
+                        Continuar comprando
+                      </button>
+                      <button
+                        className=" flex primary-button w-1/2 text-xs text-left items-center"
+                        onClick={goToCartHandler}
+                      >
+                        <p>Ir al carrito</p> &nbsp;
+                        <BsCart2 className="text-2xl" />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              )}
+              {isOutOfStock && (
+                <form className="text-center mt-3 ">
+                  <label className="mt-3 font-bold ">
+                    Únete a nuestra lista de espera
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Nombre"
+                    className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                  />
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                  />
+                  <button className="primary-button mt-3" type="submit">
+                    Enviar
+                  </button>
+                </form>
+              )}
+            </div>
           </div>
         </div>
       </div>
